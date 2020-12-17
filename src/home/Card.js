a22ea5c31e8d7808821e621ebbb50652b538d442
Jeslove <jerryjeslove@gmail.com>
2020-12-17 16:02:29 +0000

    m

--- src/home/Card.js
+++ src/home/Card.js
@@ -6,8 +6,8 @@ class Card extends Component{
 			<div className="bg-dark-gray mv4 mw5 mh3 dib br3 pa3 grow bw2 shadow-5">
 				<img src="https://robohash.org/homecoming" alt="" className="db w-100 br2 br--top imgs" />
 				<div>
-				<h4 className="tc white">Frank Jeslove</h4>
-				<p className="tc white">jeslove@gmail.com</p>
+					<h4 className="tc white">Frank Jeslove</h4>
+					<p className="tc white">jeslove@gmail.com</p>
 				</div>
 			</div>
 		);
