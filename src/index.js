39da4d6d5f8f5d9f5a16b0014aeb20ec580eca1f
Jeslove <jerryjeslove@gmail.com>
2020-12-17 16:00:40 +0000

    modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './home/Hello';
+import Card from './home/Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello />
+    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/robots.js
@@ -0,0 +1,8 @@
+export const rotots =[
+	{
+		id:1,
+		name:'Frank Jeslove',
+		username: 'Bret',
+		email:'jerryfrank@gmail.com'
+	}
+];
